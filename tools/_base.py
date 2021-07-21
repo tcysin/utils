5a17731a1b1572639143c18a9b4fbcdc1945db7b
Aleksei Tcysin <24791800+tcysin@users.noreply.github.com>
2021-07-21 16:00:32 +0300

    Add load_coco and save_coco helper routines

---
+++ tools/_base.py
@@ -0,0 +1,35 @@
+import json
+
+
+def load_coco(path):
+    """
+    Load JSON file with Coco annotations and return
+    `(images, annotations, categories, licenses, info)` tuple.
+    """
+
+    # load json with COCO annotations
+    with open(path, 'r') as fin:
+        coco = json.load(fin)
+
+    images = coco.get('images')
+    annotations = coco.get('annotations')
+    categories = coco.get('categories')
+    licenses = coco.get('licenses')
+    info = coco.get('info')
+
+    return images, annotations, categories, licenses, info
+
+
+def save_coco(path, images, annotations, categories, licenses, info):
+    """Save new Coco annotation file to given destination path."""
+
+    data = {
+        'info': info,
+        'images': images,
+        'annotations': annotations,
+        'categories': categories,
+        'licenses': licenses,
+    }
+
+    with open(path, 'w', encoding='UTF-8') as fout:
+        json.dump(data, fout, indent=1)
