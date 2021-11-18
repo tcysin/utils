70019759a83222bd25a5cd0afb7bea0fab4af210
Aleksei Tcysin <24791800+tcysin@users.noreply.github.com>
2021-11-18 14:53:13 +0300

    Refactor object cropping script

--- tools/crop_objects_yolo.py
+++ tools/crop_objects_yolo.py
@@ -5,6 +5,7 @@ import yaml
 from collections import defaultdict
 from math import inf
 from pathlib import Path
+from typing import List
 
 from PIL import Image
 from tqdm import tqdm
@@ -13,8 +14,30 @@ from tqdm import tqdm
 IMAGE_EXTENSIONS = (".jpg", ".jpeg", ".png")
 
 
+def get_files(path, exts):
+    """Return sorted list of absolute paths to files in the tree rooted at path.
+
+    Only return the files with given extensions.
+    """
+
+    result = []
+
+    for dirpath, _, filenames in os.walk(path):
+        selected = [fn for fn in filenames if fn.lower().endswith(exts)]
+        abspath = Path(dirpath).resolve()
+        paths = (abspath / fn for fn in selected)
+        result.extend(paths)
+
+    return sorted(result)
+
+
+def get_image_files(path) -> List[Path]:
+    """Return sorted list of absolute paths to image files in the tree rooted at path."""
+    return get_files(path, exts=IMAGE_EXTENSIONS)
+
+
 def clamp(x, smallest=-inf, largest=inf):
-    """Return x clamped to the [smallest, largest] interval."""
+    """Return x clamped to the given interval."""
     return max(smallest, min(x, largest))
 
 
@@ -43,6 +66,14 @@ def to_xyxy_abs(box_xywh_rel, width, height):
 
 
 def load_yolo_predictions(txt_path, classes=None):
+    """Return a list of records from YOLO prediction file.
+
+    Each record is a list `[label, box, score]`, where the box contains
+    coordinates in YOLOv5 format. `score` can be None if text file does
+    not contain scores, and `label` is either integer, or string if the
+    mapping `classes` is provided.
+    """
+
     result = []
 
     for line in open(txt_path, "r"):
@@ -56,28 +87,6 @@ def load_yolo_predictions(txt_path, classes=None):
         result.append([label, box, score])
 
     return result
-
-
-def get_files(path, exts):
-    """Return sorted list of absolute paths to files in the tree rooted at path.
-
-    Only return the files with given extensions.
-    """
-
-    result = []
-
-    for dirpath, _, filenames in os.walk(path):
-        selected = [fn for fn in filenames if fn.lower().endswith(exts)]
-        abspath = Path(dirpath).resolve()
-        paths = (abspath / fn for fn in selected)
-        result.extend(paths)
-
-    return sorted(result)
-
-
-def get_image_files(path):
-    """Return sorted list of absolute paths to image files in the tree rooted at path."""
-    return get_files(path, exts=IMAGE_EXTENSIONS)
 
 
 def get_parser():
@@ -127,33 +136,44 @@ def get_parser():
     return parser
 
 
-if __name__ == "__main__":
-    # get CL arguments
-    parser = get_parser()
-    args = parser.parse_args()
+def crop_objects_yolo(
+    data_dir: Path,
+    labels_dir: Path,
+    out_dir: Path,
+    yaml_path: Path = None,
+    folder="rois",
+    file="filename2objects.json",
+    pad=0,
+    prefix="",
+    verbose=False,
+):
 
     # prepare output directory for roi crops
-    rois_dir = args.out_dir / args.folder
+    rois_dir = out_dir / folder
     rois_dir.mkdir()
 
     # load classes from dataset.yaml, if any
-    if args.yaml_path:
-        with open(args.yaml_path) as f:
+    if yaml_path:
+        with open(yaml_path) as f:
             yaml_dict = yaml.safe_load(f)
-            classes = yaml_dict["names"]
+            classes = yaml_dict.get("names")
     else:
         classes = None
 
     # set up the procedure
     filename2objects = defaultdict(list)
     obj_id = 1
-    fnames = get_image_files(args.data_dir)
+
+    # get paths to images in given directory
+    fnames = get_image_files(data_dir)
+    assert fnames, f"no images in {data_dir}"
 
     # extract ROIs
-    for image_path in tqdm(fnames):
+    wrapper = tqdm if verbose else iter
+    for image_path in wrapper(fnames):
         # get path to txt predictions file for this image
         txt_name = image_path.stem + ".txt"
-        txt_path = args.labels_dir / txt_name
+        txt_path = labels_dir / txt_name
 
         # load YOLOv5 predictions
         output = load_yolo_predictions(txt_path, classes)
@@ -168,10 +188,8 @@ if __name__ == "__main__":
 
             # crop roi and save it
             left, upper, right, lower = box_xyxy
-            im_crop = im.crop(
-                (left - args.pad, upper - args.pad, right + args.pad, lower + args.pad)
-            )
-            name_obj = f"{args.prefix}{obj_id}_{label}.jpg"
+            im_crop = im.crop((left - pad, upper - pad, right + pad, lower + pad))
+            name_obj = f"{prefix}{obj_id}_{label}.jpg"
             im_crop.save(rois_dir / name_obj)
 
             # create an entry for this object: [id, box_xyxy, score, label_name]
@@ -180,8 +198,26 @@ if __name__ == "__main__":
             obj_id += 1
 
     # save predictions file
-    with open(args.out_dir / args.file, 'w') as fout:
+    with open(out_dir / file, "w") as fout:
         json.dump(filename2objects, fout, indent=1)
 
-    N = len(get_image_files(rois_dir))
-    print(f"Extracted {N} ROIs.")
+    if verbose:
+        N = len(get_image_files(rois_dir))
+        print(f"Extracted {N} ROIs.")
+
+
+if __name__ == "__main__":
+    parser = get_parser()
+    args = parser.parse_args()
+
+    crop_objects_yolo(
+        args.data_dir,
+        args.labels_dir,
+        args.out_dir,
+        args.yaml_path,
+        args.folder,
+        args.file,
+        args.pad,
+        args.prefix,
+        verbose=True,
+    )
